499ffbc4c6e5dbde8d3f7da0e822a4b984c6d5ac
LordWalace <walaceestudante@gmail.com>
2025-04-22 20:05:04 -0300

    Javadoc concluido e declaração de não plagio adicionada

--- src/DiarioCultural.java
+++ src/DiarioCultural.java
@@ -493,3 +493,16 @@ public class DiarioCultural {
                 .collect(Collectors.toList());
     }
 }
+
+/******************************************************************************************
+
+ Autor: Walace de Jesus Venas
+ Componente Curricular: EXA863 MI-PROGRAMAÇÃO
+ Concluído em: 21/04/2025
+ Declaro que este código foi elaborado por mim de forma individual e não contêm nenhum
+ trecho de código de outro colega ou de outro autor, tais como provindos de livros e
+ apostilas, e páginas ou documentos eletrônicos da Internet. Qualquer trecho de código
+ de outra autoria que não a minha está destacado com uma citação para o autor e a fonte
+ do código, e estou ciente que estes trechos não serão considerados para fins de avaliação.
+
+ *******************************************************************************************/
--- src/Interface.java
+++
@@ -1,3 +0,0 @@
-// === Interface.java === "View"
-
-// Vazio por enquanto essa parte da interface e a terceira entrega(FrontEnd)
